8d570c09f3b3106537d19ad1c0529d03fca59b50
yuyu <swharriet2012@gmail.com>
2023-11-08 23:04:01 +1100

    convert age to interger

--- bouncer.py
+++ bouncer.py
@@ -1,9 +1,12 @@
 age = input('Enter your age: ')
 
 if age:
-    if int(age) >= 18 and int(age) < 21:
+    age = int(age)
+    # if int(age) >= 18 and int(age) < 21:
+    if age >= 18 and age < 21:
         print('You can enter, but need a wristband!')
-    elif int(age) >= 21:
+    # elif int(age) >= 21:
+    elif age >= 21:
         print('You are good to enter and can drink!')
     else:
         print('You can\'t come in, little one!')
