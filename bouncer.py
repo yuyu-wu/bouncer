820ba34c45591461abae4c5fa4575e45ac388a68
yuyu <swharriet2012@gmail.com>
2023-11-08 23:06:56 +1100

    cleaner conditions

--- bouncer.py
+++ bouncer.py
@@ -2,12 +2,16 @@ age = input('Enter your age: ')
 
 if age:
     age = int(age)
-    # if int(age) >= 18 and int(age) < 21:
-    if age >= 18 and age < 21:
-        print('You can enter, but need a wristband!')
-    # elif int(age) >= 21:
-    elif age >= 21:
+    # # if int(age) >= 18 and int(age) < 21:
+    # if age >= 18 and age < 21:
+    #     print('You can enter, but need a wristband!')
+    # # elif int(age) >= 21:
+    # elif age >= 21:
+    #     print('You are good to enter and can drink!')
+    if age >= 21:
         print('You are good to enter and can drink!')
+    elif age >= 18:
+        print('You can enter, but need a wristband!')
     else:
         print('You can\'t come in, little one!')
 else: 
